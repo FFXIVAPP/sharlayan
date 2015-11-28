9ce0e1612d304a8323514dbf65c8a64136e0b225
Ryan Wilson <syndicated.life@gmail.com>
2015-11-27 21:39:16 -0800

    move all caching from main app into memory library
    this is currently only for actor entity, moving on to partyinfo

--- FFXIVAPP.Memory/Reader.Actor.cs
+++ FFXIVAPP.Memory/Reader.Actor.cs
@@ -28,7 +28,9 @@
 // POSSIBILITY OF SUCH DAMAGE. 
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using FFXIVAPP.Memory.Core;
 using FFXIVAPP.Memory.Core.Enums;
 using FFXIVAPP.Memory.Delegates;
@@ -36,11 +38,39 @@ using FFXIVAPP.Memory.Helpers;
 
 namespace FFXIVAPP.Memory
 {
+    public class ActorReadResult
+    {
+        public ActorReadResult()
+        {
+            PreviousMonster = new Dictionary<uint, uint>();
+            PreviousNPC = new Dictionary<uint, uint>();
+            PreviousPC = new Dictionary<uint, uint>();
+
+            NewMonster = new List<uint>();
+            NewNPC = new List<uint>();
+            NewPC = new List<uint>();
+        }
+
+        public ConcurrentDictionary<uint, ActorEntity> MonsterEntities => MonsterWorkerDelegate.EntitiesDictionary;
+
+        public ConcurrentDictionary<uint, ActorEntity> NPCEntities => NPCWorkerDelegate.EntitiesDictionary;
+
+        public ConcurrentDictionary<uint, ActorEntity> PCEntities => PCWorkerDelegate.EntitiesDictionary;
+
+        public Dictionary<uint, uint> PreviousMonster { get; set; }
+        public Dictionary<uint, uint> PreviousNPC { get; set; }
+        public Dictionary<uint, uint> PreviousPC { get; set; }
+
+        public List<UInt32> NewMonster { get; set; }
+        public List<UInt32> NewNPC { get; set; }
+        public List<UInt32> NewPC { get; set; }
+    }
+
     public static partial class Reader
     {
-        public static List<ActorEntity> GetActors()
+        public static ActorReadResult GetActors()
         {
-            var entities = new List<ActorEntity>();
+            var result = new ActorReadResult();
 
             if (MemoryHandler.Instance.SigScanner.Locations.ContainsKey("CHARMAP"))
             {
@@ -74,12 +104,13 @@ namespace FFXIVAPP.Memory
                     #endregion
 
                     var endianSize = MemoryHandler.Instance.ProcessModel.IsWin64 ? 8 : 4;
+
                     const int limit = 1372;
 
                     var characterAddressMap = MemoryHandler.Instance.GetByteArray(MemoryHandler.Instance.SigScanner.Locations["CHARMAP"], endianSize * limit);
-
                     var uniqueAddresses = new Dictionary<IntPtr, IntPtr>();
                     var firstAddress = IntPtr.Zero;
+
                     var firstTime = true;
 
                     for (var i = 0; i < limit; i++)
@@ -108,13 +139,94 @@ namespace FFXIVAPP.Memory
 
                     #region ActorEntity Handlers
 
+                    result.PreviousMonster = MonsterWorkerDelegate.EntitiesDictionary.Keys.ToDictionary(key => key);
+                    result.PreviousNPC = NPCWorkerDelegate.EntitiesDictionary.Keys.ToDictionary(key => key);
+                    result.PreviousPC = PCWorkerDelegate.EntitiesDictionary.Keys.ToDictionary(key => key);
+
                     foreach (var kvp in uniqueAddresses)
                     {
                         try
                         {
                             var source = MemoryHandler.Instance.GetByteArray(new IntPtr(kvp.Value.ToInt64()), 0x23F0);
+                            //var source = MemoryHandler.Instance.GetByteArray(characterAddress, 0x3F40);
+
+                            UInt32 ID;
+                            UInt32 NPCID2;
+                            Actor.Type Type;
+
+                            switch (MemoryHandler.Instance.GameLanguage)
+                            {
+                                case "Chinese":
+                                    ID = BitConverter.ToUInt32(source, 0x74);
+                                    NPCID2 = BitConverter.ToUInt32(source, 0x80);
+                                    Type = (Actor.Type) source[0x8A];
+                                    break;
+                                default:
+                                    ID = BitConverter.ToUInt32(source, 0x74);
+                                    NPCID2 = BitConverter.ToUInt32(source, 0x80);
+                                    Type = (Actor.Type) source[0x8A];
+                                    break;
+                            }
+
+                            ActorEntity existing = null;
+                            var newEntry = false;
+
+                            switch (Type)
+                            {
+                                case Actor.Type.Monster:
+                                    if (result.PreviousMonster.ContainsKey(ID))
+                                    {
+                                        result.PreviousMonster.Remove(ID);
+                                        existing = MonsterWorkerDelegate.GetEntity(ID);
+                                    }
+                                    else
+                                    {
+                                        result.NewMonster.Add(ID);
+                                        newEntry = true;
+                                    }
+                                    break;
+                                case Actor.Type.PC:
+                                    if (result.PreviousPC.ContainsKey(ID))
+                                    {
+                                        result.PreviousPC.Remove(ID);
+                                        existing = PCWorkerDelegate.GetEntity(ID);
+                                    }
+                                    else
+                                    {
+                                        result.NewPC.Add(ID);
+                                        newEntry = true;
+                                    }
+                                    break;
+                                case Actor.Type.NPC:
+                                    if (result.PreviousNPC.ContainsKey(NPCID2))
+                                    {
+                                        result.PreviousNPC.Remove(NPCID2);
+                                        existing = NPCWorkerDelegate.GetEntity(NPCID2);
+                                    }
+                                    else
+                                    {
+                                        result.NewNPC.Add(NPCID2);
+                                        newEntry = true;
+                                    }
+                                    break;
+                                default:
+                                    if (result.PreviousNPC.ContainsKey(ID))
+                                    {
+                                        result.PreviousNPC.Remove(ID);
+                                        existing = NPCWorkerDelegate.GetEntity(ID);
+                                    }
+                                    else
+                                    {
+                                        result.NewNPC.Add(ID);
+                                        newEntry = true;
+                                    }
+                                    break;
+                            }
+
                             var isFirstEntry = kvp.Value.ToInt64() == firstAddress.ToInt64();
-                            var entry = ActorEntityHelper.ResolveActorFromBytes(source, isFirstEntry);
+
+                            var entry = ActorEntityHelper.ResolveActorFromBytes(source, isFirstEntry, existing);
+
                             entry.MapIndex = mapIndex;
                             if (isFirstEntry)
                             {
@@ -134,36 +246,46 @@ namespace FFXIVAPP.Memory
                                     entry.TargetID = (int) currentTargetID;
                                 }
                             }
-
                             if (!entry.IsValid)
+                            {
+                                result.NewMonster.Remove(entry.ID);
+                                result.NewMonster.Remove(entry.NPCID2);
+                                result.NewNPC.Remove(entry.ID);
+                                result.NewNPC.Remove(entry.NPCID2);
+                                result.NewPC.Remove(entry.ID);
+                                result.NewPC.Remove(entry.NPCID2);
+                                continue;
+                            }
+                            if (existing != null)
                             {
                                 continue;
                             }
 
-                            switch (entry.Type)
+                            if (newEntry)
                             {
-                                case Actor.Type.Monster:
-                                    MonsterWorkerDelegate.EnsureEntity(entry.ID, entry);
-                                    break;
-                                case Actor.Type.PC:
-                                    PCWorkerDelegate.EnsureEntity(entry.ID, entry);
-                                    break;
-                                case Actor.Type.NPC:
-                                    NPCWorkerDelegate.EnsureEntity(entry.NPCID2, entry);
-                                    break;
-                                default:
-                                    NPCWorkerDelegate.EnsureEntity(entry.ID, entry);
-                                    break;
+                                switch (entry.Type)
+                                {
+                                    case Actor.Type.Monster:
+                                        MonsterWorkerDelegate.EnsureEntity(entry.ID, entry);
+                                        break;
+                                    case Actor.Type.PC:
+                                        PCWorkerDelegate.EnsureEntity(entry.ID, entry);
+                                        break;
+                                    case Actor.Type.NPC:
+                                        NPCWorkerDelegate.EnsureEntity(entry.NPCID2, entry);
+                                        break;
+                                    default:
+                                        NPCWorkerDelegate.EnsureEntity(entry.ID, entry);
+                                        break;
+                                }
                             }
-
-                            entities.Add(entry);
                         }
                         catch (Exception ex)
                         {
                         }
                     }
 
-                    MemoryHandler.ScanCount++;
+                    MemoryHandler._scanCount++;
 
                     #endregion
                 }
@@ -172,7 +294,7 @@ namespace FFXIVAPP.Memory
                 }
             }
 
-            return entities;
+            return result;
         }
     }
 }
