c8ad5f67f38e9a9506797e1ad8a610328e82f46c
Ryan Wilson <syndicated.life@gmail.com>
2016-02-24 20:27:05 -0800

    3.2 updates, resources

--- FFXIVAPP.Memory/Properties/AssemblyInfo.cs
+++ FFXIVAPP.Memory/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿// FFXIVAPP.Memory ~ AssemblyInfo.cs
 // 
-// Copyright © 2007 - 2015 Ryan Wilson - All Rights Reserved
+// Copyright © 2007 - 2016 Ryan Wilson - All Rights Reserved
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SyndicatedLife")]
 [assembly: AssemblyProduct("FFXIVAPP.Memory")]
-[assembly: AssemblyCopyright("Copyright © 2007 - 2015 Ryan Wilson")]
+[assembly: AssemblyCopyright("Copyright © 2007 - 2016 Ryan Wilson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
