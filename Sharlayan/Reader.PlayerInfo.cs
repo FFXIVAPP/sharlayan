cdc6945b457a03823ab934436586603d2ec4836d
Ryan Wilson <syndicated.life@gmail.com>
2021-11-23 14:35:54 -0800

    handle null issues on player entity

--- Sharlayan/Reader.PlayerInfo.cs
+++ Sharlayan/Reader.PlayerInfo.cs
@@ -1,10 +1,10 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Reader.CurrentPlayer.cs" company="SyndicatedLife">
+// <copyright file="Reader.PlayerInfo.cs" company="SyndicatedLife">
 //   Copyright© 2007 - 2021 Ryan Wilson <syndicated.life@gmail.com> (https://syndicated.life/)
 //   Licensed under the MIT license. See LICENSE.md in the solution root for full license information.
 // </copyright>
 // <summary>
-//   Reader.CurrentPlayer.cs Implementation
+//   Reader.PlayerInfo.cs Implementation
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -13,20 +13,10 @@ namespace Sharlayan {
 
     using Sharlayan.Core;
     using Sharlayan.Models.ReadResults;
-    using Sharlayan.Utilities;
 
     public partial class Reader {
-        private bool CanGetPlayerInfo() {
-            bool canRead = this._memoryHandler.Scanner.Locations.ContainsKey(Signatures.PLAYERINFO_KEY);
-            if (canRead) {
-                // OTHER STUFF?
-            }
-
-            return canRead;
-        }
-
-        private bool CanGetCurrentUser() {
-            bool canRead = this._memoryHandler.Scanner.Locations.ContainsKey(Signatures.CHARMAP_KEY);
+        public bool CanGetPlayerInfo() {
+            bool canRead = this._memoryHandler.Scanner.Locations.ContainsKey(Signatures.CHARMAP_KEY) && this._memoryHandler.Scanner.Locations.ContainsKey(Signatures.PLAYERINFO_KEY);
             if (canRead) {
                 // OTHER STUFF?
             }
@@ -36,65 +26,6 @@ namespace Sharlayan {
 
         public CurrentPlayerResult GetCurrentPlayer() {
             CurrentPlayerResult result = new CurrentPlayerResult();
-
-            result.Entity = this.GetCurrentPlayerEntity();
-            result.PlayerInfo = this.GetPlayerInfo();
-
-            return result;
-        }
-
-        private ActorItem GetCurrentPlayerEntity() {
-            ActorItem result = null;
-
-            if (!this.CanGetCurrentUser() || !this._memoryHandler.IsAttached) {
-                return result;
-            }
-
-            IntPtr targetAddress = IntPtr.Zero;
-
-            int limit = this._memoryHandler.Structures.ActorItem.EntityCount;
-            int sourceSize = this._memoryHandler.Structures.ActorItem.SourceSize;
-
-            byte[] characterAddressMap = this._memoryHandler.BufferPool.Rent(8);
-            byte[] sourceMap = this._memoryHandler.BufferPool.Rent(sourceSize);
-            byte[] targetInfoMap = this._memoryHandler.BufferPool.Rent(128);
-
-            try {
-                this._memoryHandler.GetByteArray(this._memoryHandler.Scanner.Locations[Signatures.CHARMAP_KEY], characterAddressMap);
-
-                IntPtr characterAddress = new IntPtr(SharlayanBitConverter.TryToInt64(characterAddressMap, 0));
-
-                if (characterAddress != IntPtr.Zero) {
-                    this._memoryHandler.GetByteArray(characterAddress, sourceMap);
-
-                    result = this._actorItemResolver.ResolveActorFromBytes(sourceMap, true);
-
-                    (uint mapID, uint mapIndex, uint mapTerritory) = this.GetMapInfo();
-
-                    result.MapID = mapID;
-                    result.MapIndex = mapIndex;
-                    result.MapTerritory = mapTerritory;
-
-                    if (targetAddress.ToInt64() > 0) {
-                        this._memoryHandler.GetByteArray(targetAddress, targetInfoMap);
-                        result.TargetID = (int) SharlayanBitConverter.TryToUInt32(targetInfoMap, this._memoryHandler.Structures.ActorItem.ID);
-                    }
-                }
-            }
-            catch (Exception ex) {
-                this._memoryHandler.RaiseException(Logger, ex);
-            }
-            finally {
-                this._memoryHandler.BufferPool.Return(characterAddressMap);
-                this._memoryHandler.BufferPool.Return(sourceMap);
-                this._memoryHandler.BufferPool.Return(targetInfoMap);
-            }
-
-            return result;
-        }
-
-        private PlayerInfo GetPlayerInfo() {
-            PlayerInfo result = new PlayerInfo();
 
             if (!this.CanGetPlayerInfo() || !this._memoryHandler.IsAttached) {
                 return result;
@@ -112,7 +43,7 @@ namespace Sharlayan {
                 this._memoryHandler.GetByteArray(playerInfoAddress, playerMap);
 
                 try {
-                    result = this._playerInfoResolver.ResolvePlayerFromBytes(playerMap);
+                    result.PlayerInfo = this._playerInfoResolver.ResolvePlayerFromBytes(playerMap);
                 }
                 catch (Exception ex) {
                     this._memoryHandler.RaiseException(Logger, ex);
@@ -132,11 +63,19 @@ namespace Sharlayan {
                                 Enmity = this._memoryHandler.GetUInt32(address + this._memoryHandler.Structures.EnmityItem.Enmity),
                             };
                             if (agroEntry.ID > 0) {
-                                result.EnmityItems.Add(agroEntry);
+                                result.PlayerInfo.EnmityItems.Add(agroEntry);
                             }
                         }
                     }
                 }
+
+                if (this._pcWorkerDelegate.CurrentUser == null) {
+                    // this is called as not all implementors will use the actor reader
+                    // actor reader is required to have been called once to get .Entity
+                    this.GetActors();
+                }
+
+                result.Entity = this._pcWorkerDelegate.CurrentUser;
             }
             catch (Exception ex) {
                 this._memoryHandler.RaiseException(Logger, ex);
